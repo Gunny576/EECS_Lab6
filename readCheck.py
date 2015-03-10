f163a7da0ffad2580f7bc8b5c78ae6e82ba456ec
Austin Sievert <arsievert1212@gmail.com>
2015-03-09 23:54:29 -0500

    readcheck v 1.1

--- readCheck.py
+++ readCheck.py
@@ -1,6 +1,8 @@
 # code to read in the files
 # this code is just basic stuff to test how to read in from a csv file
+# this code now also checks to make sure the matrix's are valid
 import csv
+import numpy as np
 
 
 file1 = open('input1.csv', 'r')  # reads in the first file
@@ -26,3 +28,29 @@ x = matrix1[1][0]+matrix1[1][0]
 print(x)
 print(matrix1)
 print(matrix2)
+
+
+validFlag1 = 1
+validFlag2 = 1
+
+for row in matrix1:
+    for row1 in matrix1:
+        if len(row) != len(row1):
+            validFlag1 = 0
+
+for row in matrix2:
+    for row1 in matrix2:
+        if len(row) != len(row1):
+            validFlag2 = 0
+
+if validFlag1 == 0:
+    print("Input from Input1.csv is not a valid Matrix")
+
+if validFlag2 == 0:
+    print("Input from Input2.csv is not a valid Matrix")
+
+array1 = np.asarray(matrix1)
+array2 = np.asarray(matrix2)
+
+print(array1)
+print(array2)
