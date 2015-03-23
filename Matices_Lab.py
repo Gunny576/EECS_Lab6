3b1ab4bf06f2acc45d32074c9037efccb8b69365
Seth Brouhard <sbrouhar@1005d-10.eecs.ku.edu>
2015-03-23 15:48:49 -0500

    Corrected the coding in this file

--- Matices_Lab.py
+++ Matices_Lab.py
@@ -1,28 +1,61 @@
 def add_Mat(mat_a,mat_b) :
 	
 	a = len(mat_a)
-	c = len(mat_a[0])
-	b = len(mat_b)
+	b = len(mat_a[0])
+	c = len(mat_b)
 	d = len(mat_b[0])
-	if a == b and c == d:
+	if a == c and b == d:
 		
-		C = [ [ 0 for r in range(c) ] for s in range(a) ]
+		mat_Result = [ [ 0 for r in range(b) ] for s in range(a) ]
 				
 				
-		for i in range(len(A)):
-			for j in range(len(A[0])):
-				result = A[i][j] + B[i][j]
+		for i in range (a):
+			for j in range (b):
+				mat_Result[i][j] = mat_a[i][j] + mat_b[i][j]
 						
-				C[i][j] = (result)
 				
-		print C
+				
+		print mat_Result
+		
 
 	else:
 		print "Matrices don't have same rows or columns"
 		
+
+def sub_Mat(mat_a,mat_b) :
+
+	a = len(mat_a)
+	b = len(mat_a[0])
+	c = len(mat_b)
+	d = len(mat_b[0])
+	
+	if a == c and b == d:
+
+		mat_Result = [ [ 0 for r in range(b) ] for s in range(a) ]
+
+
+		for i in range (a):
+			for j in range (b):
+				mat_Result[i][j] = mat_a[i][j] * -1 + mat_b[i][j]
+      
+      		 
+
+		print mat_Result
+
+	else:
+		print "Matrices don't have same rows or columns"
+
+
+
+
+
 		
-A = [[1,2], [3, 4],[2,4],[5,7]]
-B = [[1,2], [3, 4],[1,2],[3,4]]
+A = [[1,2], [3, 4],[4,4],[5,7]]
+B = [[1,2], [3, 4],[4,2],[3,4]]
+
+
+
 
 
 add_Mat(A,B)
+sub_Mat(A,B)
