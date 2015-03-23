ea438a5135b016bd714f1e9481e2dea37dd2a611
Seth Brouhard <sbrouhar@1005d-10.eecs.ku.edu>
2015-03-23 16:47:59 -0500

    final message

--- readCheck.py
+++ readCheck.py
@@ -37,8 +37,6 @@ def matrixValidator(matrix):
     return validFlag
 
 
-array1 = fileReader(file1)
-array2 = fileReader(file2)
 
 
 def transpose(matrix):
@@ -46,6 +44,106 @@ def transpose(matrix):
     for i in range (len(matrix)):
         for j in range (len(matrix[0])):
             TransposeMatrix[j][i] = matrix[i][j]
-    print(TransposeMatrix)    
+    print TransposeMatrix    
     return TransposeMatrix
+    
+    
+
+def add_Mat(mat_a,mat_b) :
+	
+	a = len(mat_a)
+	b = len(mat_a[0])
+	c = len(mat_b)
+	d = len(mat_b[0])
+	if a == c and b == d:
+		
+		mat_Result = [ [ 0 for r in range(b) ] for s in range(a) ]
+				
+				
+		for i in range (a):
+			for j in range (b):
+				mat_Result[i][j] = mat_a[i][j] + mat_b[i][j]
+						
+				
+				
+		print mat_Result
+		
+
+	else:
+		print "Matrices don't have same rows or columns"
+		
+
+def sub_Mat(mat_a,mat_b) :
+
+	a = len(mat_a)
+	b = len(mat_a[0])
+	c = len(mat_b)
+	d = len(mat_b[0])
+	
+	if a == c and b == d:
+
+		mat_Result = [ [ 0 for r in range(b) ] for s in range(a) ]
+
+
+		for i in range (a):
+			for j in range (b):
+				mat_Result[i][j] = mat_a[i][j] * -1 + mat_b[i][j]
+      
+      		 
+
+		print mat_Result
+
+	else:
+		print "Matrices don't have same rows or columns"
+		
+		
+def mult(a,b):
+	rowa = len(a)
+	cola = len(a[0])
+	rowb = len(b)
+	colb = len(b[0])
+	if cola == rowb:
+		print('Running')
+		result = [ [ 0 for r in range(colb) ] for s in range(rowa) ]
+		for i in range(len(a)):
+  			for j in range(len(b[0])):
+      				 for k in range(len(b)):
+           				result[i][j] += a[i][k] * b[k][j]
+		print result
+
+
+
+
+
+
+
+
+array1 = fileReader(file1)
+array2 = fileReader(file2)
+
+
+
+
+matrixValidator(array1)
+matrixValidator(array2)
+
+print "Try to add matrices"
+
+add_Mat(array1,array2)
+
+print "Try to subtract matrices"
+
+sub_Mat(array1,array2)
+
+print "Try to multiply matrices"
+
+mult(array1,array2)
+
+print "Transpose matrix 1"
+
+transpose(array1)
+
+print  "Transpose matrix 2"
+
+transpose(array2)
 
